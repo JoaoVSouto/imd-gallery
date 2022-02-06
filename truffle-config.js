d3da86dff6b49811214ee58fa25ed71ba80e1274
João Vítor <joaovsoutosantos@gmail.com>
2022-02-06 15:38:12 -0300

    chore: deploy contract to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = process.env.ROPSTEN_MNEMONIC;
+const rpc_url = `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_API_KEY}`;
+
 module.exports = {
   networks: {
     development: {
       host: '172.18.48.1',
       port: 7545,
       network_id: '*',
+    },
+    ropsten: {
+      provider() {
+        return new HDWalletProvider(mnemonic, rpc_url);
+      },
+      network_id: '3',
     },
   },
   contracts_directory: './src/contracts/',
