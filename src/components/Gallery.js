72f4347183988dbc7a69f8c9da9acdcecd9fabda
João Vítor <joaovsoutosantos@gmail.com>
2022-02-06 19:30:10 -0300

    style: improve gallery responsivity

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,9 +9,14 @@ export function Gallery() {
   const { posts, handleProposalAdd, handleProposalRemoval } = useApp();
 
   return (
-    <SimpleGrid my={8} minChildWidth={['240px', '310px']} gap={6}>
+    <SimpleGrid
+      my={8}
+      minChildWidth={['100%', '310px']}
+      gap={6}
+      justifyItems={{ base: 'center', md: 'start' }}
+    >
       {posts.map(post => (
-        <GridItem key={post.hash} w={['240px', '310px']}>
+        <GridItem key={post.hash} w={['100%', '310px']}>
           <Post
             {...post}
             onAddProposal={handleProposalAdd}
